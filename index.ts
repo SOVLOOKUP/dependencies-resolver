762fe5f0b333d2833870cf31820d5da3cba56be3
sovlookup <gonorth@qq.com>
2021-06-21 09:57:25 +0800

    style: 完善注释和代码风格

--- index.ts
+++ index.ts
@@ -11,100 +11,96 @@ import fetch from 'cross-fetch'
 import konan = require('konan')
 const url = 'https://nodejs.org/docs/latest/api/documentation.json'
 
-function pprint(content: any[]) {
+function pprint(...content: any[]) {
   console.log(' [ dependencies-resolver ] ', ...content)
 }
 
+export interface dependencyJson {
+  [packageName: string]: string
+}
+
 /**
- * 文件遍历方法
+ * 从文件或文件夹中递归获取依赖
  * @param {string} filePath 需要遍历的文件路径
- * @returns {Array<string>} fileList 文件列表
+ * @param {string[]} extend 搜索的拓展名
+ * @returns {string[]} fileList 文件列表
  */
-function getFiles(filePath: string): Array<string> {
+export function getDepends(path: string, extend: string[]): string[] {
   const fileList = []
+  const depends = []
   function findFile(path: string) {
     const files = readdirSync(path)
     files.forEach((item) => {
       const fPath = join(path, item)
       const stat = statSync(fPath)
-
       if (stat.isDirectory() === true && item !== 'node_modules') {
         findFile(fPath)
       }
-
-      if (stat.isFile() === true && (item.endsWith(".js") || item.endsWith(".ts") || item.endsWith(".jsx") || item.endsWith(".mjs") || item.endsWith(".cjs"))) {
+      const newExtend = extend.map((ext) => item.endsWith('.' + ext))
+      if (stat.isFile() === true && newExtend.includes(true)) {
         fileList.push(fPath)
       }
     })
   }
-
-  findFile(filePath)
-  return fileList
-}
-
-async function getDepends(path: string): Promise<string[]> {
-  const depends = []
-  const files = getFiles(path)
-  files.forEach((item) => {
+  findFile(path)
+  fileList.forEach((item) => {
     depends.push(...konan(readFileSync(item, 'utf-8')).strings)
   })
   return Array.from(new Set(depends))
 }
 
 /**
- * 找出需要安装的包
- * @param {string} path 需要检索的文件路径
- * @param {Object<string,string>} attach 附加的依赖
- * @param {string} npmClient 安装使用的包管理器名称
+ * Search dependencies and install
+ * @param {string} path search path
+ * @param {Object<string,string>} attach attach dependencies
+ * @param {string} npmClient package install client
+ * @param {string[]} excludeOption options excluded from package.json
+ * @param {string[]} extend filter suffix of searching files
+ * @return {Promise<dependencyJson>} <dependencies,version> installed
  */
 const requireResolver = async (
   path: string,
-  attach: { [packageName: string]: string } = {},
+  attach: dependencyJson = {},
   npmClient: string = 'npm',
-  excludeOption:string[] = ['dependencies','devDependencies','scripts'],
-) => {
+  excludeOption: string[] = ['dependencies', 'devDependencies', 'scripts'],
+  extend: string[] = ['js', 'mjs', 'cjs', 'ts', 'jsx']
+): Promise<dependencyJson> => {
+  const dependencyJson: dependencyJson = {}
   const content = await (await fetch(url)).json()
   const table = content.miscs[0].miscs.filter(
     (item: { name: string }) => item.name === 'stability_overview'
   )[0].desc
-
   const internelModules = table
     .match(/<a href=(.*?)>/g)
     .map((item: string | any[]) => item.slice(9, -7))
-
-  const toinstall = (await getDepends(path))
+  const toinstall = getDepends(path, extend)
     .filter((item) => !(item.startsWith('./') || item.startsWith('../')))
     .filter((item) => !internelModules.includes(item))
-
-  pprint(['Find dependencies: ', toinstall])
-  
+  pprint('Find dependencies: ', toinstall)
   const pkgJsonPath = resolve(path, 'package.json')
   let pkgJson = new Object()
   if (existsSync(pkgJsonPath)) {
     pkgJson = JSON.parse(readFileSync(pkgJsonPath, 'utf-8'))
-
     excludeOption.forEach((item) => {
       if (pkgJson[item]) {
         delete pkgJson[item]
       }
     })
   }
-  const dependencyJson = new Object()
   toinstall.forEach((item) => {
     dependencyJson[item] = '*'
   })
   Object.keys(attach).forEach((dependency) => {
     dependencyJson[dependency] = attach[dependency]
   })
-
   pkgJson['dependencies'] = dependencyJson
-
   writeFileSync(pkgJsonPath, JSON.stringify(pkgJson))
   const currentPath = resolve()
   process.chdir(path)
-  pprint(['Installing dependencies...'])
-  pprint([execSync(`${npmClient} install`).toString('utf-8')])
+  pprint('Installing dependencies...')
+  pprint(execSync(`${npmClient} install`).toString('utf-8'))
   process.chdir(currentPath)
+  return dependencyJson
 }
 
 export default requireResolver
